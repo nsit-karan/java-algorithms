b59920211ed3daa07f3d934dda2d727e37d96511
karan arora <nsit_karan@yahoo.com>
2023-05-02 20:07:15 +0530

    read a line from input using scanner

--- basic-java-programs/src/com/basics/Main.java
+++ basic-java-programs/src/com/basics/Main.java
@@ -1,7 +1,17 @@
 package com.basics;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Iterate over the command line arguments
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
     }
 }
