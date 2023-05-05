ec827905eecfa21f29e9919ecaf0b65897105a83
karan arora <nsit_karan@yahoo.com>
2023-05-05 22:06:14 +0530

    var args basic eg

--- basic-java-programs/src/com/methods/BasicMethods.java
+++ basic-java-programs/src/com/methods/BasicMethods.java
@@ -15,6 +15,35 @@ public class BasicMethods {
     public void changeArgument(int[] nums) {
         nums[0] = 99;
     }
+
+    /**
+     * Sample fun to demonstrate variable no of args
+     * args here gets translated to an array by java
+     *
+     * not, although args is implicitly an array, the function
+     * signature is different if this was declared like int[] args.
+     *
+     * the way this is called will be different. in the second case,
+     * you will have to pass in an array unlike the current case
+     * where you can pass in free flowing strings
+     *
+     * @param args
+     */
+    void varArgsFunc(int ...args) {
+        System.out.println(Arrays.toString(args));
+    }
+
+    /**
+     * basic fun with args as input
+     * its similar to varArgsFunc, just the caller code
+     * snippet will be diff
+     *
+     * @param args
+     */
+    void FuncArray(int[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+
     public static void main(String[] args) {
         BasicMethods basicMethods = new BasicMethods();
         System.out.printf("Sum is %d\n", basicMethods.sumNos(10, 30));
@@ -31,5 +60,12 @@ public class BasicMethods {
         int[] arr = {1, 3, 5, 20, 30};
         basicMethods.changeArgument(arr);
         System.out.println("Changed value is " + Arrays.toString(arr));
+
+        // variable no of args call
+        basicMethods.varArgsFunc(1, 2, 3, 4, 5, 6, 7, 8);
+
+        // this won't work. it has to be an array
+        //basicMethods.FuncArray(1, 2, 4); WONT WORK
+        basicMethods.FuncArray(arr);
     }
 }
