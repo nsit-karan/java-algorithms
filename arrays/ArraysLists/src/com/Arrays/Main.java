3b2ef1a4c8fedcbdea1f05caebe2ec2965901bd2
karan arora <nsit_karan@yahoo.com>
2023-05-06 12:12:02 +0530

    basic new array project stub with array initialization

---
+++ arrays/ArraysLists/src/com/Arrays/Main.java
@@ -0,0 +1,29 @@
+package com.Arrays;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void basicArray() {
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        /*
+         * This will print
+         * [1,2,0,0,0,0,0,0,0,0]
+         *
+         * if we dont initialize all the indexes, it will
+         * take the default for the datatype, i.e, int
+         * in this case. so, defaulted to 0
+         */
+        System.out.println(Arrays.toString(numbers));
+
+        // another way to initialize array
+        int[] newNos = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(newNos));
+    }
+    public static void main(String[] args) {
+        basicArray();
+    }
+}
