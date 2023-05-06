dc9f55d47107fa1026e510b930f5684b139662f5
karan arora <nsit_karan@yahoo.com>
2023-05-06 19:34:33 +0530

    array iteration , default values

--- ArraysList/src/BasicArrays.java
+++ ArraysList/src/BasicArrays.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Main {
+public class BasicArrays {
 
     public static void basicArray() {
         int[] numbers = new int[10];
@@ -21,8 +21,27 @@ public class Main {
         int[] newNos = {1, 2, 3, 4, 5, 6};
         System.out.println(Arrays.toString(newNos));
 
+        /*
+         * String is non-primitive so the default value is null
+         * in the below case, strings array will be full of nulls
+         * expected o/p : [null, null, null, null]
+         */
         String[] strings = new String[4];
         System.out.println(Arrays.toString(strings));
+
+        printArrayNew(newNos);
+    }
+
+    public static void printArrayOld(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void printArrayNew(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 
     public static void main(String[] args) {
