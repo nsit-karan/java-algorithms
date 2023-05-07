99fcf81a03dcf611e5207f65f87c44e94f11427c
karan arora <nsit_karan@yahoo.com>
2023-05-07 20:28:09 +0530

    basic 2d array, diff size for cols, fill in array from terminal

--- ArraysList/src/MultiDimension.java
+++ ArraysList/src/MultiDimension.java
@@ -1,6 +1,21 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDimension {
+
+    public static void inputArray() {
+
+        System.out.println("Input the array elements 3x2");
+        Scanner scanner = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = scanner.nextInt();
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+    }
     public static void main(String[] args) {
         /*
          * specifying rows is mandatory, columns isn't
@@ -15,5 +30,18 @@ public class MultiDimension {
         };
         System.out.println(Arrays.deepToString(arr1));
         System.out.println(Arrays.deepToString(arr2));
+
+        /*
+         * all the sub-arrays are of diff dimensions
+         */
+        int[][] arr3 = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9, 10, 11}
+        };
+        System.out.println(Arrays.deepToString(arr3));
+
+        // Fill in array from terminal
+        inputArray();
     }
 }
