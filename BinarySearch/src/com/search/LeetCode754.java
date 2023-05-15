b6cc590afef93c0675d385f90fe508f32d52d353
karan arora <nsit_karan@yahoo.com>
2023-05-15 22:13:22 +0530

    leet code 754

---
+++ BinarySearch/src/com/search/LeetCode754.java
@@ -0,0 +1,47 @@
+package com.search;
+
+public class LeetCode754 {
+
+    /**
+     * You are given an array of characters letters that is sorted in
+     * non-decreasing order, and a character target. There are at least
+     * two different characters in letters.
+     *
+     * Return the smallest character in letters that is lexicographically
+     * greater than target. If such a character does not exist, return the
+     * first character in letters.
+     *
+     * Approach :
+     * - leverage the binary search logic for finding ceiling of a no
+     * - caveats being that if no is not found, return the first element
+     * 
+     * @param letters
+     * @param target
+     * @return
+     */
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        if (target >= letters[letters.length -1]) {
+            return letters[0];
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else if (target > letters[mid]) {
+                start = mid + 1;
+            } else if (target == letters[mid]) {
+                start = mid + 1;
+            }
+
+        }
+        return letters[start];
+    }
+    public static void main(String[] args) {
+        char[] characters = {'c','f','j'};
+        System.out.println(nextGreatestLetter(characters, 'g'));
+    }
+}
