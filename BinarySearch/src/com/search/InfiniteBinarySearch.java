be520eda49ab191b8afabf5840c9af3dc74af24e
karan arora <nsit_karan@yahoo.com>
2023-05-17 22:28:16 +0530

    stub code for searching for an element in infinite array

---
+++ BinarySearch/src/com/search/InfiniteBinarySearch.java
@@ -0,0 +1,14 @@
+package com.search;
+
+/**
+ * Assume that the input is an infinite sorted array. Find the location
+ * for a given target input
+ *
+ * by infinite, it implies that we dont know the start/end of the array
+ * and can't use the length function
+ */
+public class InfiniteBinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
