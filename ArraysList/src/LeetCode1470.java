3554618ab04504b39de8b579bb4c2734db2caccf
karan arora <nsit_karan@yahoo.com>
2023-05-18 22:13:31 +0530

    leetcode 1470

---
+++ ArraysList/src/LeetCode1470.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class LeetCode1470 {
+
+    /**
+     * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+     * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+     *
+     * TODO - optimize the memory usage and avoid using a new array.
+     * 
+     * @param nums
+     * @param n
+     * @return
+     */
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2*n];
+        for (int i = 0, j = n, k = 0; j < 2*n; i++, j++) {
+            result[k] = nums[i];
+            result[k + 1] = nums[j];
+            k = k + 2;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        System.out.println(Arrays.toString(shuffle(nums, 3)));
+    }
+}
