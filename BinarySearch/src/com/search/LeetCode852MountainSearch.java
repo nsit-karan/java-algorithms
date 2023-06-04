7990c3903af258e3a64d46f783a2dfc26116029a
karan arora <nsit_karan@yahoo.com>
2023-06-04 18:51:32 +0530

    fixed mountain search algo

--- BinarySearch/src/com/search/LeetCode852MountainSearch.java
+++ BinarySearch/src/com/search/LeetCode852MountainSearch.java
@@ -4,33 +4,33 @@ public class LeetCode852MountainSearch {
 
     public static int peakIndexInMountainArray(int[] arr) {
 
-        int start = 0;
-        int end = arr.length - 1;
+            int start = 0;
+            int end = arr.length - 1;
 
-        while (start < end) {
-            int mid = start + (end - start)/2;
+            while (start < end) {
+                int mid = start + (end - start)/2;
 
-            /*
-             * it means, we are in the increasing part of the array
-             * and the ans will lie towards the right of mid
-             */
-            if (arr[mid] < arr[mid + 1]) {
-                start = mid + 1;
-            } else if (arr[mid] > arr[mid + 1]) {
                 /*
-                 * it means, we are in the decreasing part of the array
-                 * and the ans will lie towards the left of the array
-                 *
-                 * we also include mid here since that could be a potential ans
-                 * since mid could be the highest no (try drawing this and
-                 * you will follow why mid also needs to be included)
+                 * it means, we are in the increasing part of the array
+                 * and the ans will lie towards the right of mid
                  */
-                end = mid;
+                if (arr[mid] < arr[mid + 1]) {
+                    start = mid + 1;
+                } else if (arr[mid] > arr[mid + 1]) {
+                    /*
+                     * it means, we are in the decreasing part of the array
+                     * and the ans will lie towards the left of the array
+                     *
+                     * we also include mid here since that could be a potential ans
+                     * since mid could be the highest no (try drawing this and
+                     * you will follow why mid also needs to be included)
+                     */
+                    end = mid;
+                }
             }
-        }
 
-        // at the end , start == end
-        return start;
+            // at the end , start == end
+            return start;
 
     }
 
